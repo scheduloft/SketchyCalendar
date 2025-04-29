2e4199e680f3c5177eba9d93f80c4a03cdd3e752
Paul Sonnentag <paul.sonnentag@googlemail.com>
2025-04-29 11:47:26 +0200

    remove server config
    ... because it was messing up the auth stuff

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,4 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default {
   plugins: [tsconfigPaths()],
-  server: {
-    host: true,
-    headers: {
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp",
-    },
-    allowedHosts: ["marcel.local"],
-  },
 };
